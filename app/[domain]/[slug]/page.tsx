17898de6b76d15ebd25f807686eceaf3272b5b77
Me <suraj@chaibuilder.com>
2023-09-20 14:39:08 +0530

    feat: export const dynamicParams = true  added

--- app/[domain]/[slug]/page.tsx
+++ app/[domain]/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { fetchRouteSnapshot, RenderBlocks } from "@/app/app/(dashboard)/editor/next";
 
+export const dynamicParams = true;
 export default async function Page({ params }: { params: { domain: string; slug: string } }) {
   const snapshot: any = await fetchRouteSnapshot(params.slug, params.domain);
   return <RenderBlocks snapshot={snapshot} />;
