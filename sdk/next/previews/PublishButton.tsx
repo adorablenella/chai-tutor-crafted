84d4ec44078f49d4dc79b6396d1561a709970b6e
Krishna Gopal Patidar <67382167+kgpatidar@users.noreply.github.com>
2023-09-27 13:21:24 +0530

    fix: disable publish on not saved state with info tooltip (#15)

--- sdk/next/previews/PublishButton.tsx
+++ sdk/next/previews/PublishButton.tsx
@@ -1,8 +1,8 @@
 import { find } from "lodash";
-import { Button } from "../../package";
+import { Button, Tooltip, TooltipContent, TooltipTrigger } from "../../package";
 import { usePages } from "../hooks/usePages";
 import { usePublishPage } from "../mutations/usePageActions";
-import { useCurrentPage } from "../store";
+import { useCurrentPage, useSyncState } from "../store";
 import { useProject } from "../hooks/useProject";
 
 const PublishButton = () => {
@@ -10,14 +10,32 @@ const PublishButton = () => {
   const [currentPageUid] = useCurrentPage();
   const { mutate, isLoading } = usePublishPage();
   const { data: project } = useProject();
+  const [syncStatus] = useSyncState();
+
   const publishPage = () => {
     const page: { slug: string } = find(pages, { uuid: currentPageUid }) as { slug: string };
     const slug = currentPageUid === project?.homepage ? "_home" : page.slug;
     mutate(slug);
   };
+
+  if (syncStatus !== "SAVED") {
+    return (
+      <Tooltip delayDuration={500}>
+        <TooltipTrigger asChild>
+          <Button className="cursor-not-allowed opacity-50">{isLoading ? "Publishing..." : "Publish"}</Button>
+        </TooltipTrigger>
+        {syncStatus !== "SAVED" && (
+          <TooltipContent className="mr-4">
+            <div>Save changes to publish</div>
+          </TooltipContent>
+        )}
+      </Tooltip>
+    );
+  }
+
   return (
     <div>
-      <Button disabled={isLoading} onClick={publishPage}>
+      <Button disabled={isLoading || syncStatus !== "SAVED"} onClick={publishPage}>
         {isLoading ? "Publishing..." : "Publish"}
       </Button>
     </div>
