0b5da3d52b031c5825b554f86d4f25d1120ebe04
Krishna Gopal Patidar <67382167+kgpatidar@users.noreply.github.com>
2023-09-28 10:12:50 +0530

    chore: added 50 most used google fonts (#16)

--- sdk/package/components/sidepanels/panels/branding/BrandingOptions.tsx
+++ sdk/package/components/sidepanels/panels/branding/BrandingOptions.tsx
@@ -6,12 +6,66 @@ import { useBrandingOptions } from "../../../../hooks";
 import { Color, Numeric, SelectOption } from "../../../../controls/controls";
 import { useBuilderProp } from "@/sdk/package/hooks/useBuilderProp";
 
+const FONTS = [
+  { title: "Roboto", value: "Roboto" },
+  { title: "Open Sans", value: "Open Sans" },
+  { title: "Montserrat", value: "Montserrat" },
+  { title: "Lato", value: "Lato" },
+  { title: "Poppins", value: "Poppins" },
+  { title: "Oswald", value: "Oswald" },
+  { title: "Raleway", value: "Raleway" },
+  { title: "Ubuntu", value: "Ubuntu" },
+  { title: "Nunito", value: "Nunito" },
+  { title: "Merriweather", value: "Merriweather" },
+  { title: "Nunito Sans", value: "Nunito Sans" },
+  { title: "Playfair Display", value: "Playfair Display" },
+  { title: "Rubik", value: "Rubik" },
+  { title: "Inter", value: "Inter" },
+  { title: "Lora", value: "Lora" },
+  { title: "Kanit", value: "Kanit" },
+  { title: "Fira Sans", value: "Fira Sans" },
+  { title: "Hind", value: "Hind" },
+  { title: "Quicksand", value: "Quicksand" },
+  { title: "Mulish", value: "Mulish" },
+  { title: "Barlow", value: "Barlow" },
+  { title: "Inconsolata", value: "Inconsolata" },
+  { title: "Titillium Web", value: "Titillium Web" },
+  { title: "Heebo", value: "Heebo" },
+  { title: "IBM Plex Sans", value: "IBM Plex Sans" },
+  { title: "DM Sans", value: "DM Sans" },
+  { title: "Nanum Gothic", value: "Nanum Gothic" },
+  { title: "Karla", value: "Karla" },
+  { title: "Arimo", value: "Arimo" },
+  { title: "Cabin", value: "Cabin" },
+  { title: "Oxygen", value: "Oxygen" },
+  { title: "Overpass", value: "Overpass" },
+  { title: "Assistant", value: "Assistant" },
+  { title: "Tajawal", value: "Tajawal" },
+  { title: "Play", value: "Play" },
+  { title: "Exo", value: "Exo" },
+  { title: "Cinzel", value: "Cinzel" },
+  { title: "Faustina", value: "Faustina" },
+  { title: "Philosopher", value: "Philosopher" },
+  { title: "Gelasio", value: "Gelasio" },
+  { title: "Sofia Sans Condensed", value: "Sofia Sans Condensed" },
+  { title: "Noto Sans Devanagari", value: "Noto Sans Devanagari" },
+  { title: "Actor", value: "Actor" },
+  { title: "Epilogue", value: "Epilogue" },
+  { title: "Glegoo", value: "Glegoo" },
+  { title: "Overlock", value: "Overlock" },
+  { title: "Lustria", value: "Lustria" },
+  { title: "Ovo", value: "Ovo" },
+  { title: "Suranna", value: "Suranna" },
+];
+
 const BrandingOptions = (): React.JSX.Element => {
   const onSaveBrandingOptions = useBuilderProp("onSaveBrandingOptions", async () => {});
   const [brandingOptions, setBrandingOptions] = useBrandingOptions();
   const brandingRef = React.useRef(brandingOptions);
 
-  React.useEffect(() => () => onSaveBrandingOptions(brandingRef.current), [brandingRef, onSaveBrandingOptions]);
+  React.useEffect(() => {
+    return () => onSaveBrandingOptions(brandingRef.current);
+  }, [brandingRef, onSaveBrandingOptions]);
 
   const updateRealtime = ({ formData }: IChangeEvent, id?: string) => {
     if (id) {
@@ -36,12 +90,12 @@ const BrandingOptions = (): React.JSX.Element => {
     _headingFont: SelectOption({
       title: "Heading font",
       default: _headingFont,
-      options: [{ title: _headingFont, value: _headingFont }],
+      options: FONTS,
     }),
     _bodyFont: SelectOption({
       title: "Body font",
       default: _bodyFont,
-      options: [{ title: _headingFont, value: _headingFont }],
+      options: FONTS,
     }),
     _roundedCorners: Numeric({ title: "Rounded Corner", default: parseInt(_roundedCorners, 10) }),
     _primaryColor: Color({ title: "Primary", default: _primaryColor }),
