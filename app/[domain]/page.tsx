c007374eab7d7263e0eb2805c774c299ffe3aec4
Me <suraj@chaibuilder.com>
2023-10-01 14:59:47 +0530

    feat: site rendering done using pages directory

--- app/[domain]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { RenderBlocks } from "@/sdk/next";
-
-export default async function SiteHomePage({ params }: { params: { domain: string } }) {
-  return <RenderBlocks slug={"_home"} domain={params.domain} />;
-}
---
+++ pages/[domain]/[...slug].tsx
@@ -0,0 +1,20 @@
+import { GetStaticProps } from "next";
+
+export default function Main({ slug }: any) {
+  return <div>Render: {slug}</div>;
+}
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  return {
+    props: {
+      slug: context.params?.slug,
+    },
+  };
+};
