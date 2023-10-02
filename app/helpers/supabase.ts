b4d83944d5fe98b6dd31734c8fa39d37c27acc7c
Me <suraj@chaibuilder.com>
2023-10-02 15:15:23 +0530

    fix: suppress auth error

--- app/helpers/supabase.ts
+++ app/helpers/supabase.ts
@@ -3,6 +3,10 @@ import { createClient, SupabaseClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
+const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: false,
+  },
+});
 
 export default supabase;
