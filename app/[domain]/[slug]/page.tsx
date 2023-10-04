c80f4d04c99e27008698e3753ba287eb4565b9ac
Me <suraj@chaibuilder.com>
2023-10-04 19:07:08 +0530

    feat: slug pages fixed

--- app/[domain]/[slug]/page.tsx
+++ app/[domain]/[slug]/page.tsx
@@ -1,8 +1,28 @@
 import { fetchRouteSnapshot, RenderBlocks } from "@/sdk/next";
 import { notFound } from "next/navigation";
+import { get } from "lodash";
 
+export async function generateMetadata({ params }: { params: { domain: string; slug: string } }) {
+  const snapshot = await fetchRouteSnapshot(params.slug, params.domain);
+  return {
+    title: get(snapshot, "pageData.seo_data.title", ""),
+    description: get(snapshot, "pageData.seo_data.description", ""),
+    openGraph: {
+      title: get(snapshot, "pageData.seo_data.title", ""),
+      description: get(snapshot, "pageData.seo_data.description", ""),
+      type: "website",
+    },
+    themeColor: get(snapshot, "projectData.branding_options._primaryColor", ""),
+    twitter: {
+      card: "summary_large_image",
+      title: get(snapshot, "pageData.seo_data.title", ""),
+      description: get(snapshot, "pageData.seo_data.description", ""),
+      images: [get(snapshot, "pageData.seo_data.image", "")],
+    },
+  };
+}
 export default async function Page({ params }: { params: { domain: string; slug: string } }) {
-  const snapshot: any = await fetchRouteSnapshot(params.slug, params.domain);
+  const snapshot = await fetchRouteSnapshot(params.slug, params.domain);
   if (snapshot.notFound) {
     return notFound();
   }
