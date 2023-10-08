0aede1a5b428409c4be683081d03b01df8de0e78
Me <suraj@chaibuilder.com>
2023-10-08 21:04:37 +0530

    feat: added dropdown block

--- custom-blocks/index.ts
+++ custom-blocks/index.ts
@@ -1,4 +1,5 @@
 import "@/custom-blocks/preline/navbars/Navbar1";
 import "@/custom-blocks/preline/navbars/Navbar2";
 import "@/custom-blocks/preline/hero/Hero1";
+import "@/custom-blocks/preline/overlays/DropDown";
 import "./client";
