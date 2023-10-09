850ed18ed7c6174779407c5d09438003e4b0e57f
Krishna Gopal Patidar <67382167+kgpatidar@users.noreply.github.com>
2023-10-09 13:44:21 +0530

    UI Library (#33)
    * feat: added library in supabase storage
    
    * chore: added predefined blocks to supabase

--- sdk/next/hooks/useUiLibrary.ts
+++ sdk/next/hooks/useUiLibrary.ts
@@ -1,8 +1,10 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
+const BUCKET = "chaibuilder-blob-storage";
+const BUCKET_API = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/${BUCKET}`;
 
 export const useUiLibraryBlocks = () => {
   return useMutation(async () => {
-    const res = await fetch("/library.json").then((res) => res.json());
+    const res = await fetch(`${BUCKET_API}/predefined-blocks/library.json?t=2023-10-09`).then((res) => res.json());
     return res;
   });
 };
@@ -11,17 +13,10 @@ export const useExternalPredefinedBlock = () => {
   return useMutation(async (block: any) => {
     if (!block || !block.uuid) return null;
     try {
-      const res = await fetch(`/blocks/${block.uuid}.json`).then((res) => res.json());
-      return res;
+      const res = await fetch(`${BUCKET_API}/predefined-blocks/blocks/${block.uuid}.json`).then((res) => res.json());
+      return res || [];
     } catch (error) {
-      return {
-        name: "Head 1",
-        uuid: "7acb25c7-9e88-40cc-b83f-2d5d42eae54f",
-        blocks: [{ _type: "Heading", content: "Head 1", level: "h1", _id: "a" }],
-        html: "",
-        group: "Header",
-        preview: "https://fakeimg.pl/200x100?text=Head+1&font=bebas",
-      };
+      return [];
     }
   });
 };
--- sdk/package/hooks/useUiLibraries.ts
+++ sdk/package/hooks/useUiLibraries.ts
@@ -2,12 +2,15 @@ import { useBuilderProp } from "@/sdk/package/hooks/useBuilderProp";
 import { useEffect, useState } from "react";
 
 export const useUILibraryBlocks = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [blocks, setBlocks] = useState([]);
   const getBlocks = useBuilderProp("getUILibraryBlocks", () => []);
   useEffect(() => {
     (async () => {
+      setIsLoading(true);
       setBlocks(await getBlocks());
+      setIsLoading(false);
     })();
   }, []);
-  return blocks;
+  return { data: blocks, isLoading };
 };
