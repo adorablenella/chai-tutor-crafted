a2cefd78343cef44ff69a06b7b39af3253edfeda
Me <suraj@chaibuilder.com>
2023-10-13 18:08:25 +0530

    chore: remove divider content

--- sdk/next/blocks/basic/divider.tsx
+++ sdk/next/blocks/basic/divider.tsx
@@ -1,9 +1,8 @@
 import * as React from "react";
 import { DividerHorizontalIcon } from "@radix-ui/react-icons";
 import { registerChaiBlock } from "@/sdk/next/server";
-import { MultilineText, Styles } from "@/sdk/package/controls/controls";
+import { Styles } from "@/sdk/package/controls/controls";
 import { TBlock } from "@/sdk/package/types/TBlock";
-import { isEmpty } from "lodash";
 
 /**
  * Divider component
@@ -11,17 +10,7 @@ import { isEmpty } from "lodash";
  * @constructor
  */
 const DividerBlock = (props: TBlock & { blockProps: Record<string, string>; _styles: Record<string, string> }) => {
-  const { blockProps, _styles, _content } = props;
-  if (!isEmpty(_content)) {
-    return (
-      <div {...blockProps} className="inline-flex w-full items-center justify-center">
-        <hr {..._styles} />
-        <span
-          dangerouslySetInnerHTML={{ __html: _content }}
-          className="absolute left-1/2 -translate-x-1/2 bg-white px-3 font-medium text-gray-900 dark:bg-gray-900 dark:text-white"></span>
-      </div>
-    );
-  }
+  const { blockProps, _styles } = props;
   return React.createElement("hr", { ..._styles, ...blockProps });
 };
 
@@ -33,6 +22,5 @@ registerChaiBlock(DividerBlock as React.FC<any>, {
   group: "basic",
   props: {
     _styles: Styles({ default: "bg-gray-900 h-0.5 py-2 my-1" }),
-    _content: MultilineText({ title: "Divider label html", default: "Label" }),
   },
 });
