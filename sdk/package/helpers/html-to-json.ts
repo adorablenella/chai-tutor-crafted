1edcb1cfba4c8708765cf677ca2dcc2f9d3f3ad3
Krishna Gopal Patidar <67382167+kgpatidar@users.noreply.github.com>
2023-10-20 16:03:37 +0530

    fix: remove style attr and height/width for svg (#1)

--- sdk/package/helpers/html-to-json.ts
+++ sdk/package/helpers/html-to-json.ts
@@ -1,14 +1,14 @@
 // @ts-ignore
 import { parse, stringify } from "himalaya";
 import { generateUUID } from "../functions/functions";
-import { capitalize, find, flatMapDeep, flatten, forEach, get, includes, isEmpty, set } from "lodash";
+import { capitalize, filter, find, flatMapDeep, flatten, forEach, get, includes, isEmpty, set } from "lodash";
 import { TBlock } from "../types";
 import { STYLES_KEY } from "@/sdk/package/constants/CONTROLS";
 
 type Node = {
   type: "element" | "text" | "comment";
   tagName: string;
-  attributes: Record<string, string>;
+  attributes: Array<Record<string, string>>;
   children: Node[];
 };
 
@@ -91,7 +91,7 @@ const getAttrs = (node: Node) => {
 
   forEach(attributes, ({ key, value }) => {
     if (replacers[key]) set(attrs, replacers[key], getSanitizedValue(value));
-    else set(attrs, `_attrs.${key}`, getSanitizedValue(value));
+    else if (!includes(["style"], key)) set(attrs, `_attrs.${key}`, getSanitizedValue(value));
   });
 
   delete attrs.class;
@@ -249,6 +249,7 @@ const traverseNodes = (nodes: Node[], parent: any = null): TBlock[] => {
        * handling svg tag
        * if svg tag just pass html stringify content as _icon
        */
+      node.attributes = filter(node.attributes, (attr) => !includes(["style", "width", "height"], attr.key));
       block._icon = stringify([node]);
       return [block] as TBlock[];
     } else if (node.tagName == "option" && parent && parent.block?._type === "Select") {
