ed249effe679e2d56438895c7fb4f230812c9505
Me <suraj@chaibuilder.com>
2023-10-21 12:13:02 +0530

    chore: added w-full as safelisted classes

--- styles/tailwind.site.config.js
+++ styles/tailwind.site.config.js
@@ -30,7 +30,7 @@ colors.textDarkMode = TEXT_DARK_MODE;
 module.exports = {
   darkMode: "class",
   content: ["./custom-blocks/**/*.{js,ts,jsx,tsx,mdx}", "node_modules/preline/dist/*.js"],
-  safelist: ["min-h-screen", "h-full"],
+  safelist: ["min-h-screen", "h-full", "w-full"],
   theme: {
     extend: {
       borderRadius: {
