b1d2138c6084c1a798905d6ac06f17ae58b8e698
Me <suraj@chaibuilder.com>
2023-10-28 08:36:39 +0530

    chore: xyz domain

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,25 +1,7 @@
 import "@/styles/site.css";
 import "@/custom-blocks";
 import { ReactNode } from "react";
-import { notFound, redirect } from "next/navigation";
-import { getSiteByDomain } from "../helpers/project";
 
-export default async function SiteLayout({ params, children }: { params: { domain: string }; children: ReactNode }) {
-  const domain = "new." + process.env.NEXT_PUBLIC_ROOT_DOMAIN;
-  const data = await getSiteByDomain(domain);
-
-  if (!data) {
-    return notFound();
-  }
-
-  // Optional: Redirect to custom domain if it exists
-  if (
-    domain.endsWith(`.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) &&
-    data.customDomain &&
-    process.env.REDIRECT_TO_CUSTOM_DOMAIN_IF_EXISTS === "true"
-  ) {
-    return redirect(`https://${data.customDomain}`);
-  }
-
+export default async function SiteLayout({ children }: { children: ReactNode }) {
   return <>{children}</>;
 }
