17ba412afcbcac3fb5a5e0c13b1169836a368393
Suraj Air <suraj@chaibuilder.com>
2023-10-29 14:18:55 +0530

    chore: chnaged border to outline

--- sdk/next/blocks/helper-components/empty-slot.tsx
+++ sdk/next/blocks/helper-components/empty-slot.tsx
@@ -12,7 +12,7 @@ const EmptySlot = ({
 }) => {
   return (
     <div {...blockProps} className={cn("flex h-20 flex-col items-center justify-center", className)}>
-      <div className="flex h-full w-full items-center justify-center border-2 border-dashed border-gray-400 bg-[radial-gradient(#CCC,transparent_1.5px)] duration-300 [background-size:12px_12px] dark:bg-[radial-gradient(#AAA,transparent_1.5px)]">
+      <div className="flex h-full w-full items-center justify-center bg-[radial-gradient(#CCC,transparent_1.5px)] outline-dashed outline-2 outline-gray-400 duration-300 [background-size:12px_12px] dark:bg-[radial-gradient(#AAA,transparent_1.5px)]">
         <span className="opacit flex items-center gap-x-1.5 text-xs font-semibold text-gray-400">
           <PlusCircle size={13} /> ADD {text}
         </span>
