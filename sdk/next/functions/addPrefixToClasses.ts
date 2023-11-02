4857423d8421205fd5f6e3a42369e2804ca1de9c
Suraj Air <suraj@chaibuilder.com>
2023-11-02 08:09:11 +0530

    fix: exclude paddle classes from prefix

--- sdk/next/functions/addPrefixToClasses.ts
+++ sdk/next/functions/addPrefixToClasses.ts
@@ -7,7 +7,7 @@ export const addPrefixToClasses = (classes: string, prefix: string = "c-") => {
     const classes = item.split(" ");
     const newClasses = classes.map((item) => {
       if (item === "") return "";
-      if (includes(item, "hs-") || includes(item, "[--")) return item;
+      if (includes(item, "hs-") || includes(item, "[--") || includes(item, "paddle")) return item;
       // if the class had a state of media query, then prefix the classes
       // eg: dark:hover:bg-red-500 => dark:hover:c-bg-red-500
       // eg: hover:bg-red-500 => hover:c-bg-red-500
