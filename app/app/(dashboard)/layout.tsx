b563e20545f984aece84d994c53b85a96abe9f84
Suraj Air <suraj@chaibuilder.com>
2023-11-03 18:20:47 +0530

    chore:

--- app/app/(dashboard)/layout.tsx
+++ app/app/(dashboard)/layout.tsx
@@ -6,19 +6,22 @@ import { Providers } from "@/app/app/(dashboard)/providers";
 import { ChaiScripts } from "./scripts";
 import { getUserLastTransaction } from "@/lib/actions";
 import Script from "next/script";
+import { getSession } from "@/lib/auth";
 
 export const dynamic = "force-dynamic";
 
-function isProductionEnv() {
-  return process.env.NODE_ENV === "production";
-}
 export default async function DashboardLayout({ children }: { children: ReactNode }) {
   const lastTransaction = await getUserLastTransaction();
+  const session = await getSession();
   const canUpgrade = lastTransaction === null;
+  const globalData = `{
+    name: '${session?.user?.user_metadata?.name}',
+    email: '${session?.user?.email}',
+  }`;
   return (
     <>
       <Providers>
-        <div className="flex h-screen w-screen">
+        <div {...{ "x-data": globalData }} className="flex h-screen w-screen">
           <Nav canUpgrade={canUpgrade}>
             <Suspense fallback={<div>Loading...</div>}>
               <Profile />
