626b74cd0f39dce3b536cce02741ec1081546ba9
Suraj Air <suraj@chaibuilder.com>
2023-11-03 08:59:54 +0530

    chore: added smooth-scroll to classes

--- styles/tailwind.site.config.js
+++ styles/tailwind.site.config.js
@@ -29,10 +29,12 @@ colors.textDarkMode = TEXT_DARK_MODE;
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./custom-blocks/**/*.{js,ts,jsx,tsx,mdx}", 
-            "./sdk/next/blocks/**/*.{js,ts,jsx,tsx,mdx}", 
-            "node_modules/preline/dist/*.js"],
-  safelist: ["min-h-screen", "h-full", "w-full"],
+  content: [
+    "./custom-blocks/**/*.{js,ts,jsx,tsx,mdx}",
+    "./sdk/next/blocks/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/preline/dist/*.js",
+  ],
+  safelist: ["min-h-screen", "scroll-smooth", "h-full", "w-full"],
   theme: {
     extend: {
       borderRadius: {
